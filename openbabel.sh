7d4820d15c91c48936f6c7bb448b4e9d9836d01d
Christoph Helma <helma@in-silico.ch>
2011-02-09 19:41:33 +0100

    fixed directory chang in openbabel.sh

--- openbabel.sh
+++ openbabel.sh
@@ -2,6 +2,7 @@
 
 echo "Installing OpenBabel libraries"
 . /etc/profile
+dir=`pwd`
 cd /tmp
 wget -O - "http://downloads.sourceforge.net/project/openbabel/openbabel/2.2.3/openbabel-2.2.3.tar.gz?use_mirror=kent" | tar zxv
 cd openbabel-2.2.3/
@@ -11,3 +12,4 @@ cd scripts/ruby/
 ruby extconf.rb --with-openbabel-include=/usr/local/include/openbabel-2.0
 make install
 ldconfig
+cd $dir
