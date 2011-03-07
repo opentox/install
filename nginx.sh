a6f62f2b87d5dbba9e5a11448cea86c1aa006faf
Christoph Helma <helma@in-silico.ch>
2011-03-07 13:49:53 +0100

    ruby enterprise and redis updated

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 . /etc/profile
 passenger-install-nginx-module --auto-download --auto --prefix=/opt/nginx
 
-cd /opt/ruby-enterprise-1.8.7-2010.02/lib/ruby/gems/1.8/gems/
+cd /opt/ruby-enterprise-1.8.7-2010.03/lib/ruby/gems/1.8/gems/
 passenger=`ls -d passenger*`;
 cd -
 servername=`hostname`.`dnsdomainname`
