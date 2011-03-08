15cd67433b6b57277c2da0efcf25c90be0513c1e
Christoph Helma <helma@in-silico.ch>
2011-03-08 11:49:42 +0100

    ruby instaler path fixed

--- ruby.sh
+++ ruby.sh
@@ -3,7 +3,7 @@
 echo "Installing Ruby Enterprise"
 cd /tmp
 wget -O - "http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise-1.8.7-2011.03.tar.gz" | tar zxv
-ruby-enterprise-1.8.7-2010.03/installer  --dont-install-useful-gems --no-dev-docs --auto=/opt/ruby-enterprise-1.8.7-2010.03
+sh /tmp/ruby-enterprise-1.8.7-2010.03/installer  --dont-install-useful-gems --no-dev-docs --auto=/opt/ruby-enterprise-1.8.7-2010.03
 sed -i '/^PATH=.*ruby-enterprise/d' /etc/profile
 echo 'PATH=$PATH:/opt/ruby-enterprise-1.8.7-2010.03/bin' | tee -a /etc/profile
 . /etc/profile
