79e608cc3777130fc575acf6cdac8e20f7b84a38
Andreas Maunz <andreas@maunz.de>
2011-03-29 10:31:07 +0200

    Base install not as root

--- base-install.sh
+++ base-install.sh
@@ -4,16 +4,16 @@
 # Author: Andreas Maunz
 #
 
-if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root" 1>&2
+if [ "$(id -u)" = "0" ]; then
+  echo "This script must not be run as root" 1>&2
   exit 1
 fi
 
 # Utils
-APTITUDE=`which aptitude`
-APT_CACHE=`which apt-cache`
-DCSS=`which debconf-set-selections`
-DPKG=`which dpkg`
+APTITUDE="sudo `which aptitude`"
+APT_CACHE="`which apt-cache`"
+DCSS="sudo `which debconf-set-selections`"
+DPKG="`which dpkg`"
 
 if [ ! -e $APTITUDE ]; then
   echo "Aptitude missing. Install aptitude first." 1>&2
