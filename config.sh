424abba2902b71ce95b07f550f2c2841d13265ee
Andreas Maunz <andreas@maunz.de>
2011-03-29 12:58:21 +0200

    Added config.sh

---
+++ config.sh
@@ -0,0 +1,16 @@
+# Dest
+JAVA_CONF=$HOME/.bash_java_ot
+
+RUBY_VER="ruby-enterprise-1.8.7-2011.03"
+RUBY_CONF=$HOME/.bash_ruby_ot
+RUBY_DEST="$HOME/$RUB_YVER"
+
+OB_NUM_VER="2.2.3"
+OB_VER="openbabel-$OB_NUM_VER"
+OB_DEST="$HOME/$OB_VER"
+
+KL_VER="0.9-11"
+KL_CONF="$HOME/.bash_R_ot"
+KL_DEST="$HOME/r-packages"
+
+NGINX_DEST=$HOME/nginx
