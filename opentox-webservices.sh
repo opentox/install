88295381838bb14cdb6ae9f61816f53d5e8ce489
Andreas Maunz <andreas@maunz.de>
2011-04-19 00:38:13 -0700

    Fixed arch

--- opentox-webservices.sh
+++ opentox-webservices.sh
@@ -70,7 +70,7 @@ cmd="$GIT submodule init" && run_cmd "$cmd" "Fminer Init"
 cmd="$GIT submodule update" && run_cmd "$cmd" "Fminer Update"
 for mylib in bbrc last; do
   cmd="sed -i 's,^INCLUDE_OB.*,INCLUDE_OB\ =\ -I$OB_DEST/include/openbabel-2.0,g' $WWW_DEST/opentox/algorithm/libfminer/lib$mylib/Makefile; sed -i 's,^LDFLAGS_OB.*,LDFLAGS_OB\ =\ -L$OB_DEST/lib,g' $WWW_DEST/opentox/algorithm/libfminer/lib$mylib/Makefile" && run_cmd "$cmd" "Makefile $mylib (OB)"
-  cmd="sed -i 's,^INCLUDE_RB.*,INCLUDE_RB\ =\ -I$RUBY_DEST/lib/ruby/1.8/i686-linux,g' $WWW_DEST/opentox/algorithm/libfminer/lib$mylib/Makefile" && run_cmd "$cmd" "Makefile $mylib (RB)"
+  cmd="sed -i 's,^INCLUDE_RB.*,INCLUDE_RB\ =\ -I$RUBY_DEST/lib/ruby/1.8/`uname -m`-linux,g' $WWW_DEST/opentox/algorithm/libfminer/lib$mylib/Makefile" && run_cmd "$cmd" "Makefile $mylib (RB)"
 done
 cd "libfminer/libbbrc">>$LOG 2>&1
 $GIT checkout master >>$LOG 2>&1
