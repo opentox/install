6f799b8fb74e662970f8f5a2112a4a75cbcc5ead
Andreas Maunz <andreas@maunz.de>
2012-07-17 08:10:21 +0200

    Updated installer

--- base-install.sh
+++ base-install.sh
@@ -17,8 +17,7 @@ fi
 # Utils
 check_utils "aptitude git apt-cache dpkg"
 
-# Init main file
-touch "$OT_UI_CONF"
+touch $OT_UI_CONF
 
 # Pkgs
 packs="binutils build-essential cmake curl gnuplot hostname libcurl4-openssl-dev libgsl0-dev libopenbabel4 libopenbabel-dev libraptor1-dev libreadline6-dev libreadline-dev libsqlite3-dev libssl-dev libyaml-dev libxml2-dev libxslt1-dev lsb-release openjdk-6-jdk psmisc pwgen raptor-utils r-base r-base-core r-base-dev sqlite3 udev wget xsltproc zlib1g-dev"
@@ -28,20 +27,9 @@ echo "Base Packages:"
 
 echo
 echo "Updating index"
-sudo "$APTITUDE" update -y >/dev/null 2>&1
+sudo $APTITUDE update -y >/dev/null 2>&1
 
-echo
-echo "Checking installation:"
-pack_arr=""
-for p in $packs; do
-  if [ "un" != `$DPKG -l $p 2>/dev/null | tail -1 | awk -F " " '{print $1}'` ]; then
-     printf "%30s%50s\n" $p Y
-  else
-     printf "%30s%50s\n" $p N
-     pack_arr="$pack_arr $p"
-  fi
-done
-
+pack_arr=$packs
 if [ -n "$pack_arr" ]; then
   echo 
   echo "Checking availablity:"
@@ -58,31 +46,28 @@ fi
 if [ -n "$pack_fail" ]; then
   echo 
   echo "WARNING: At least one missing package has no suitable installation candidate."
-  echo "$pack_fail"
   echo "Press <Ctrl+C> to abort (5 sec)."
   sleep 5
 fi
 
 echo
 if [ -n "$pack_arr" ]; then 
-  echo "Installing missing packages:"
+  cmd="sudo $APTITUDE -y install $pack_arr" && run_cmd "$cmd" "Installing packages"
 fi
 
-for p in $pack_arr; do
-  cmd="sudo $APTITUDE -y install $p" && run_cmd "$cmd" "$p"
-done
 
 if [ ! -f $JAVA_CONF ]; then
 
-  if [ ! -d $OT_JAVA_HOME ]; then
+  if [ ! -d "$OT_JAVA_HOME" ]; then
     echo "Directory '$OT_JAVA_HOME' does not exist! Aborting..."
     exit 1
   fi
 
-  echo 'if echo "$JAVA_HOME" | grep -v '$OT_JAVA_HOME'>/dev/null 2>&1; then export JAVA_HOME='$OT_JAVA_HOME'; fi' >> "$JAVA_CONF"
-  echo 'if echo "$PATH" | grep -v '$OT_JAVA_HOME/bin'>/dev/null 2>&1; then export PATH='$OT_JAVA_HOME/bin':"$PATH"; fi' >> "$JAVA_CONF"
-  echo 'if ! [ -d "$JAVA_HOME" ]; then echo "$0: JAVA_HOME is not a directory!"; fi' >> "$JAVA_CONF"
+  echo "if echo \"\$JAVA_HOME\" | grep -v \"$OT_JAVA_HOME\">/dev/null 2>&1; then export JAVA_HOME=\"$OT_JAVA_HOME\"; fi" >> "$JAVA_CONF"
+  echo "if echo \"\$PATH\" | grep -v \"$OT_JAVA_HOME\">/dev/null 2>&1; then export PATH=\"$OT_JAVA_HOME:\$PATH\"; fi" >> "$JAVA_CONF"
+  echo "if ! [ -d \"\$JAVA_HOME\" ]; then echo \"\$0: '\$OT_JAVA_HOME' is not a directory!\"; fi" >> "$JAVA_CONF"
 
+  echo "Java configuration has been stored in '$JAVA_CONF'."
   if ! grep "$JAVA_CONF" $OT_UI_CONF >/dev/null 2>&1; then
     echo '. '$JAVA_CONF >> $OT_UI_CONF
   fi
