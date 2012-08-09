4e899c8b2cb8f0562a4f81a6149d32900dadc305
davor <vorgrimmlerdavid@gmx.de>
2012-08-09 16:39:11 +0200

    Removed comments.

--- ot-tools-user.sh
+++ ot-tools-user.sh
@@ -5,54 +5,6 @@ otconfig() {
   source $HOME/.opentox/opentox-ui.sh
 }
 
-# Update the version
-#otupdate() {
-#  START_DIR=`pwd`
-#  otconfig
-#  cd $HOME/opentox-ruby/www/opentox
-#  for d in `find -not -name "." -type d -maxdepth 1 2>/dev/null`; do echo ; echo $d ; cd $d ; MYBRANCH=`git branch | grep "*" | sed 's/.*\ //g'`; git pull origin $MYBRANCH ; cd - ;  done
-#  cd  $HOME/opentox-ruby/www/opentox/algorithm/libfminer
-#  mv libbbrc/Makefile libbbrc/Makefile~
-#  mv liblast/Makefile liblast/Makefile~
-#  if ! git pull; then
-#    echo "Error! Pull for Fminer failed."
-#    return 1
-#  fi
-#  mv libbbrc/Makefile~ libbbrc/Makefile
-#  mv liblast/Makefile~ liblast/Makefile
-#  make -C libbbrc/ clean
-#  make -C libbbrc/ ruby
-#  make -C liblast/ clean
-#  make -C liblast/ ruby
-#  cd -
-#  cd $HOME/opentox-ruby/www/opentox/algorithm/last-utils
-#  if ! git pull; then
-#    echo "Error! Pull for Last-Utils failed."
-#    return 1
-#  fi
-#  cd -
-#  cd $HOME/opentox-ruby/www/opentox/algorithm/bbrc-sample
-#  if ! git pull; then
-#    echo "Error! Pull for Bbrc-Sample failed."
-#    return 1
-#  fi
-#  cd -
-#  cd opentox-ruby
-#  LINK_DIR=`gem which opentox-ruby | sed 's/\/opentox-ruby.rb//'`
-#  if [ -h $LINK_DIR ]; then 
-#    rm -f $LINK_DIR
-#  fi
-#  rake install
-#  if ! [ -h $LINK_DIR ]; then
-#    echo "Warning! Your lib $LINK_DIR is no symlink. Linking back for you..."
-#    rm -rf "$LINK_DIR~"
-#    mv "$LINK_DIR" "$LINK_DIR~"
-#    ln -sf $HOME/opentox-ruby/www/opentox/opentox-ruby/lib `echo ${LINK_DIR::${#LINK_DIR}-3}`
-#  fi
-#  echo "Please execute 'otstart' to restart."
-#  cd "$START_DIR"
-#}
-
 # Start the server
 otstart() {
   if [ $# != 1 ]
@@ -122,12 +74,6 @@ alias otless='less $HOME/.opentox/log/development.log'
 # Tail log
 alias ottail='tail -f $HOME/.opentox/log/development.log'
 
-# Reload the server
-#otreload() {
-  #otconfig
-  #bash -c "nginx -s reload"
-#}
-
 # Kill the server
 otkill() {
   if [ $# != 1 ]
@@ -177,12 +123,3 @@ otkill() {
       ;;
   esac
 }
-
-# Check the server
-#otcheck() {
-#  check_service=`cat $HOME/.opentox/config/production.yaml  | grep opentox-algorithm | sed 's/.*\s//g' | sed 's/"//g'`
-#  if [ -z "`curl -v $check_service 2>&1 | grep '200 OK'`" ]; then
-#    otstart
-#  fi
-#}
-
