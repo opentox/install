802917a1fbeb51d7c677dca02b443a1b251469cd
davor <vorgrimmlerdavid@gmx.de>
2013-07-08 12:52:10 +0200

    Added script to install services.

---
+++ service_install.sh
@@ -0,0 +1,24 @@
+. $HOME/.opentox/config/install/config.sh
+. $OT_PREFIX/install/utils.sh
+. $HOME/.opentox/ot-tools.sh
+otconfig
+for f in opentox-client opentox-server; do
+  git clone "https://github.com/opentox/$f.git" $OT_PREFIX/$f
+  cd $OT_PREFIX/$f
+  git checkout development 2>/dev/null
+done
+cd $OT_PREFIX/opentox-client/bin
+./opentox-client-install silent
+cd $OT_PREFIX/opentox-server/bin
+./opentox-server-install silent
+
+for f in algorithm compound dataset feature model validation task opentox-test; do
+  git clone "https://github.com/opentox/$f.git" $OT_PREFIX/$f
+  cd $OT_PREFIX/$f
+  git checkout development 2>/dev/null
+  if [ -f $OT_PREFIX/$f/bin/$f-install ]; then
+    cd $OT_PREFIX/$f/bin
+    ./$f-install silent
+  fi
+done
+notify
